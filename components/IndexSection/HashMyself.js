88dc7978d5a02ed7474ad8850ce0bcc9b69e60b8
eurobin4321 <eurobin4321@gmail.com>
2020-01-25 22:50:58 +0900

    split Index Sections with 2 components

---
+++ components/IndexSection/HashMyself.js
@@ -0,0 +1,85 @@
+import './style/HashMyself.scss';
+
+const HashMyself = props => (
+	<section id="index-hash-myself" className={props.className}>
+		<h2>Hash myself</h2>
+		<table>
+			<thead>
+				<tr>
+					<th>key</th>
+					<th>value</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>추구하는 가치</td>
+					<td>존중, 정직, 조화</td>
+				</tr>
+				<tr>
+					<td>저는...</td>
+					<td>
+						팀간의 소통을 통해 <br />
+						만들어지는 시각적 결과물을 <br />
+						만드는데 재미를 느낍니다.
+					</td>
+				</tr>
+				<tr>
+					<td>
+						개발스택 <br />& 수준
+					</td>
+					<td>
+						<dl>
+							<dt>HTML + CSS + JS</dt>
+							<dd>Fix된 디자인의 시안을 보고 Zeplin을 통해 퍼블리싱 할 수 있음</dd>
+						</dl>
+						<dl>
+							<dt>React(CRA, Next.js)</dt>
+							<dd>React와 Sass를 이용해, 컴포넌트 베이스의 재사용성 높은 개발을 할 수 있음</dd>
+						</dl>
+						<dl>
+							<dt>Node.js(Express)</dt>
+							<dd>Express를 이용하여 간단한 CRUD를 구현할 수 있음</dd>
+						</dl>
+						<dl>
+							<dt>Typescript</dt>
+							<dd>공부중 (Express와 접목하는 중)</dd>
+						</dl>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						개발툴
+						<br />& etc
+					</td>
+					<td>
+						<ul>
+							<li>VSCODE</li>
+							<li>Github</li>
+						</ul>
+					</td>
+				</tr>
+				<tr>
+					<td>디자인</td>
+					<td>
+						<dl>
+							<dt>Adobe XD with Zeplin</dt>
+							<dd>
+								간단한 수준의 플랫디자인 보조, <br />
+								Zeplin 배포가능
+							</dd>
+						</dl>
+						<dl>
+							<dt>Adobe Illustration</dt>
+							<dd>
+								디자인에 사용될 <br />
+								간단한 아이콘 제작 가능
+							</dd>
+						</dl>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+);
+
+export default HashMyself;
---
+++ components/IndexSection/index.js
@@ -0,0 +1,7 @@
+import Profile from './Profile';
+import HashMyself from './HashMyself';
+
+module.exports = {
+	Profile: Profile,
+	HashMyself: HashMyself,
+};
