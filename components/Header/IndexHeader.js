35aad0a483252d97baa8dc8e2530083d5e22249d
eurobin4321 <eurobin4321@gmail.com>
2020-01-27 01:12:59 +0900

    remove Header button

--- components/Header/IndexHeader.js
+++ components/Header/IndexHeader.js
@@ -5,7 +5,7 @@ const IndexHeader = props => (
 	<header id="index-header" className={props.className}>
 		<h1>JJAM100</h1>
 		<p>Introduction & Private place</p>
-		<Button link="/private">PRIVATE</Button>
+		{/* <Button onClick={e => alert('아직 공사중 입니다...')}>PRIVATE</Button> */}
 	</header>
 );
 
