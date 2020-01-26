baeaa0ccb102ffd977958ae7d6b180607eeb0d35
eurobin4321 <eurobin4321@gmail.com>
2020-01-27 01:11:08 +0900

    remove private page

--- pages/private.js
+++
@@ -1,9 +0,0 @@
-import { PrivateHeader } from '../components/Header';
-import { Container } from '../components/Container';
-const Private = () => (
-	<Container>
-		<PrivateHeader />
-	</Container>
-);
-
-export default Private;
